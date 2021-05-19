f1117195ecd9c2a89c9dd394e3d121d9c12c2cd2
Lauren <debruynlauren1@gmail.com>
2021-05-19 21:15:53 +0200

    Turned SymbolStrategy classes into immutable classes.

--- test_generate_strategies.py
+++ test_generate_strategies.py
@@ -7,7 +7,10 @@ import unittest
 
 
 def expected_strategy_example_function_1():
-    return {'n1': SymbolicIntegerStrategy(var_id='n1', min_value=[1])}
+    return {'n1': SymbolicIntegerStrategy(var_id='n1',
+                                          min_value=[1],
+                                          max_value=[],
+                                          filters=[])}
 
 
 def expected_strategy_example_function_2():
@@ -37,66 +40,135 @@ def expected_strategy_example_function_4():
 
 
 def expected_strategy_example_function_5():
-    return {'s': SymbolicFromRegexStrategy(var_id='s', regexps=['(+|-)?[1-9][0-9]*'], full_match=True)}
+    return {'s': SymbolicFromRegexStrategy(var_id='s',
+                                           regexps=['(+|-)?[1-9][0-9]*'],
+                                           full_match=True,
+                                           min_size=[],
+                                           max_size=[],
+                                           filters=[])}
 
 
 # TODO include dependencies
 def expected_strategy_example_function_13():
-    return {'n1': SymbolicIntegerStrategy(var_id='n1', min_value=[1, 'n3'], max_value=['n2-1']),
-            'n2': SymbolicIntegerStrategy(var_id='n2', max_value=[100]),
-            'n3': SymbolicIntegerStrategy(var_id='n3', max_value=['n2'])}
+    return {'n1': SymbolicIntegerStrategy(var_id='n1', min_value=[1, 'n3'], max_value=['n2-1'], filters=[]),
+            'n2': SymbolicIntegerStrategy(var_id='n2', min_value=[], max_value=[100], filters=[]),
+            'n3': SymbolicIntegerStrategy(var_id='n3', min_value=[], max_value=['n2'], filters=[])}
 
 
 def expected_strategy_example_function_14():
-    return {'s': SymbolicTextStrategy(var_id='s', whitelist_categories=[{'Ll', 'Lu'}])}
+    return {'s': SymbolicTextStrategy(var_id='s',
+                                      blacklist_categories=[],
+                                      whitelist_categories=[{'Ll', 'Lu'}],
+                                      min_size=[],
+                                      max_size=[],
+                                      filters=[])}
 
 
 def expected_strategy_example_function_15():
-    return {'s': SymbolicTextStrategy(var_id='s', whitelist_categories=[{'Ll', 'Lu', 'Nd'}])}
+    return {'s': SymbolicTextStrategy(var_id='s',
+                                      blacklist_categories=[],
+                                      whitelist_categories=[{'Ll', 'Lu', 'Nd'}],
+                                      min_size=[],
+                                      max_size=[],
+                                      filters=[])}
 
 
 def expected_strategy_example_function_16():
-    return {'s': SymbolicTextStrategy(var_id='s', whitelist_categories=[{'Nd'}])}
+    return {'s': SymbolicTextStrategy(var_id='s',
+                                      blacklist_categories=[],
+                                      whitelist_categories=[{'Nd'}],
+                                      min_size=[],
+                                      max_size=[],
+                                      filters=[])}
 
 
 def expected_strategy_example_function_17():
-    return {'s': SymbolicTextStrategy(var_id='s', whitelist_categories=[{'Ll'}])}
+    return {'s': SymbolicTextStrategy(var_id='s',
+                                      blacklist_categories=[],
+                                      whitelist_categories=[{'Ll'}],
+                                      min_size=[],
+                                      max_size=[],
+                                      filters=[])}
 
 
 def expected_strategy_example_function_18():
-    return {'s': SymbolicTextStrategy(var_id='s', whitelist_categories=[{'Nd', 'Nl', 'No'}])}
+    return {'s': SymbolicTextStrategy(var_id='s',
+                                      blacklist_categories=[],
+                                      whitelist_categories=[{'Nd', 'Nl', 'No'}],
+                                      min_size=[],
+                                      max_size=[],
+                                      filters=[])}
 
 
 def expected_strategy_example_function_19():
-    return {'s': SymbolicTextStrategy(var_id='s', whitelist_categories=[{'Zs'}])}
+    return {'s': SymbolicTextStrategy(var_id='s',
+                                      blacklist_categories=[],
+                                      whitelist_categories=[{'Zs'}],
+                                      min_size=[],
+                                      max_size=[],
+                                      filters=[])}
 
 
 def expected_strategy_example_function_20():
-    return {'s': SymbolicTextStrategy(var_id='s', whitelist_categories=[{'Lu'}])}
+    return {'s': SymbolicTextStrategy(var_id='s',
+                                      blacklist_categories=[],
+                                      whitelist_categories=[{'Lu'}],
+                                      min_size=[],
+                                      max_size=[],
+                                      filters=[])}
 
 
 def expected_strategy_example_function_21():
-    return {'s': SymbolicTextStrategy(var_id='s', whitelist_categories=[{'Nd'}])}
+    return {'s': SymbolicTextStrategy(var_id='s',
+                                      blacklist_categories=[],
+                                      whitelist_categories=[{'Nd'}],
+                                      min_size=[],
+                                      max_size=[],
+                                      filters=[])}
 
 
 def expected_strategy_example_function_22():
-    return {'s': SymbolicTextStrategy(var_id='s', whitelist_categories=[{'Nd'}, {'Nd', 'Nl', 'No'}, {'Nd'}])}
+    return {'s': SymbolicTextStrategy(var_id='s',
+                                      blacklist_categories=[],
+                                      whitelist_categories=[{'Nd'}, {'Nd', 'Nl', 'No'}, {'Nd'}],
+                                      min_size=[],
+                                      max_size=[],
+                                      filters=[])}
 
 
 def expected_strategy_example_function_23():
-    return {'s': SymbolicTextStrategy(var_id='s', whitelist_categories=[{'Ll', 'Lu', 'Nd'}, {'Ll', 'Lu'}])}
+    return {'s': SymbolicTextStrategy(var_id='s',
+                                      blacklist_categories=[],
+                                      whitelist_categories=[{'Ll', 'Lu', 'Nd'}, {'Ll', 'Lu'}],
+                                      min_size=[],
+                                      max_size=[],
+                                      filters=[])}
 
 
 def expected_strategy_example_function_24():
-    return {'s': SymbolicTextStrategy(var_id='s', whitelist_categories=[{'Lu'}, {'Ll'}])}
+    return {'s': SymbolicTextStrategy(var_id='s',
+                                      blacklist_categories=[],
+                                      whitelist_categories=[{'Lu'}, {'Ll'}],
+                                      min_size=[],
+                                      max_size=[],
+                                      filters=[])}
 
 
 def expected_strategy_example_function_25():
-    return {'s': SymbolicFromRegexStrategy(var_id='s', regexps=['^abc', '.*xyz$'], full_match=True)}
+    return {'s': SymbolicFromRegexStrategy(var_id='s',
+                                           regexps=['^abc', '.*xyz$'],
+                                           full_match=True, min_size=[],
+                                           max_size=[],
+                                           filters=[])}
 
 
 def expected_strategy_example_function_26():
-    return {'s': SymbolicFromRegexStrategy(var_id='s', regexps=['s33l'])}
+    return {'s': SymbolicFromRegexStrategy(var_id='s',
+                                           regexps=['s33l'],
+                                           full_match=False,
+                                           min_size=[],
+                                           max_size=[],
+                                           filters=[])}
 
 
 class GenerateSymbolTableTest(unittest.TestCase):
@@ -106,7 +178,7 @@ class GenerateSymbolTableTest(unittest.TestCase):
         self.assertEqual(expected_strategy_example_function_1(),
                          generate_strategies(strategy_factory.generate_property_table_without_failed_contracts()))
         self.assertEqual('st.integers(min_value=1)',
-                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['n1'].get_strategy())
+                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['n1'].represent())
 
     # def test_example_function_2(self) -> None:
     #     self.assertEqual(expected_strategy_example_function_2(),
@@ -117,106 +189,106 @@ class GenerateSymbolTableTest(unittest.TestCase):
         self.assertEqual(expected_strategy_example_function_5(),
                          generate_strategies(strategy_factory.generate_property_table_without_failed_contracts()))
         self.assertEqual('st.from_regex(regex=r"(+|-)?[1-9][0-9]*", fullmatch=True)',  # noqa
-                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].get_strategy())
+                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].represent())
 
     def test_example_function_13(self) -> None:
         strategy_factory = StrategyFactory(example_function_13)
         self.assertEqual(expected_strategy_example_function_13(),
                          generate_strategies(strategy_factory.generate_property_table_without_failed_contracts()))
         self.assertEqual('st.integers(min_value=max(1, n3), max_value=n2-1)',
-                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['n1'].get_strategy())
+                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['n1'].represent())
         self.assertEqual('st.integers(max_value=100)',
-                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['n2'].get_strategy())
+                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['n2'].represent())
         self.assertEqual('st.integers(max_value=n2)',
-                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['n3'].get_strategy())
+                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['n3'].represent())
 
     def test_example_function_14(self) -> None:
         strategy_factory = StrategyFactory(example_function_14)
         self.assertEqual(expected_strategy_example_function_14(),
                          generate_strategies(strategy_factory.generate_property_table_without_failed_contracts()))
         self.assertEqual('st.text(alphabet=st.characters(whitelist_categories=(\'Ll\', \'Lu\')))',
-                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].get_strategy())
+                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].represent())
 
     def test_example_function_15(self) -> None:
         strategy_factory = StrategyFactory(example_function_15)
         self.assertEqual(expected_strategy_example_function_15(),
                          generate_strategies(strategy_factory.generate_property_table_without_failed_contracts()))
         self.assertEqual('st.text(alphabet=st.characters(whitelist_categories=(\'Ll\', \'Lu\', \'Nd\')))',
-                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].get_strategy())
+                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].represent())
 
     def test_example_function_16(self) -> None:
         strategy_factory = StrategyFactory(example_function_16)
         self.assertEqual(expected_strategy_example_function_16(),
                          generate_strategies(strategy_factory.generate_property_table_without_failed_contracts()))
         self.assertEqual('st.text(alphabet=st.characters(whitelist_categories=(\'Nd\',)))',
-                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].get_strategy())
+                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].represent())
 
     def test_example_function_17(self) -> None:
         strategy_factory = StrategyFactory(example_function_17)
         self.assertEqual(expected_strategy_example_function_17(),
                          generate_strategies(strategy_factory.generate_property_table_without_failed_contracts()))
         self.assertEqual('st.text(alphabet=st.characters(whitelist_categories=(\'Ll\',)))',
-                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].get_strategy())
+                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].represent())
 
     def test_example_function_18(self) -> None:
         strategy_factory = StrategyFactory(example_function_18)
         self.assertEqual(expected_strategy_example_function_18(),
                          generate_strategies(strategy_factory.generate_property_table_without_failed_contracts()))
         self.assertEqual('st.text(alphabet=st.characters(whitelist_categories=(\'Nd\', \'Nl\', \'No\')))',
-                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].get_strategy())
+                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].represent())
 
     def test_example_function_19(self) -> None:
         strategy_factory = StrategyFactory(example_function_19)
         self.assertEqual(expected_strategy_example_function_19(),
                          generate_strategies(strategy_factory.generate_property_table_without_failed_contracts()))
         self.assertEqual('st.text(alphabet=st.characters(whitelist_categories=(\'Zs\',)))',
-                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].get_strategy())
+                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].represent())
 
     def test_example_function_20(self) -> None:
         strategy_factory = StrategyFactory(example_function_20)
         self.assertEqual(expected_strategy_example_function_20(),
                          generate_strategies(strategy_factory.generate_property_table_without_failed_contracts()))
         self.assertEqual('st.text(alphabet=st.characters(whitelist_categories=(\'Lu\',)))',
-                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].get_strategy())
+                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].represent())
 
     def test_example_function_21(self) -> None:
         strategy_factory = StrategyFactory(example_function_21)
         self.assertEqual(expected_strategy_example_function_21(),
                          generate_strategies(strategy_factory.generate_property_table_without_failed_contracts()))
         self.assertEqual('st.text(alphabet=st.characters(whitelist_categories=(\'Nd\',)))',
-                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].get_strategy())
+                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].represent())
 
     def test_example_function_22(self) -> None:
         strategy_factory = StrategyFactory(example_function_22)
         self.assertEqual(expected_strategy_example_function_22(),
                          generate_strategies(strategy_factory.generate_property_table_without_failed_contracts()))
         self.assertEqual('st.text(alphabet=st.characters(whitelist_categories=(\'Nd\',)))',
-                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].get_strategy())
+                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].represent())
 
     def test_example_function_23(self) -> None:
         strategy_factory = StrategyFactory(example_function_23)
         self.assertEqual(expected_strategy_example_function_23(),
                          generate_strategies(strategy_factory.generate_property_table_without_failed_contracts()))
         self.assertEqual('st.text(alphabet=st.characters(whitelist_categories=(\'Ll\', \'Lu\')))',
-                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].get_strategy())
+                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].represent())
 
     def test_example_function_24(self) -> None:
         strategy_factory = StrategyFactory(example_function_24)
         self.assertEqual(expected_strategy_example_function_24(),
                          generate_strategies(strategy_factory.generate_property_table_without_failed_contracts()))
         self.assertEqual('st.text(alphabet=st.characters(whitelist_categories=()))',
-                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].get_strategy())
+                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].represent())
 
     def test_example_function_25(self) -> None:
         strategy_factory = StrategyFactory(example_function_25)
         self.assertEqual(expected_strategy_example_function_25(),
                          generate_strategies(strategy_factory.generate_property_table_without_failed_contracts()))
         self.assertEqual('st.from_regex(regex=r"(?=^abc)(?=.*xyz$)", fullmatch=True)', # noqa
-                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].get_strategy())
+                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].represent())
 
     def test_example_function_26(self) -> None:
         strategy_factory = StrategyFactory(example_function_26)
         self.assertEqual(expected_strategy_example_function_26(),
                          generate_strategies(strategy_factory.generate_property_table_without_failed_contracts()))
         self.assertEqual('st.from_regex(regex=r"s33l")',
-                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].get_strategy())
+                         generate_strategies(strategy_factory.generate_property_table_without_failed_contracts())['s'].represent())
